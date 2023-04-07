460c178911f73fb845ba7c06c72d08c3e69f7ed2
sandeepajay1 <119504658+sandeepajay1@users.noreply.github.com>
2023-04-07 13:10:30 +0530

    Update k8s.tf

--- terraform_files/k8s.tf
+++ terraform_files/k8s.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "k8s-server" {
  ami = "ami-007855ac798b5175e"
  instance_type = "t2.medium"
  vpc_security_group_ids = ["sg-09947aa36d4d8ebb2"]
- subnet_ids = "subnet-05a31b67b18113593"
+ subnet_id = "subnet-05a31b67b18113593"
  key_name = "DEMOKEY"
    root_block_device {
       volume_size = 20
